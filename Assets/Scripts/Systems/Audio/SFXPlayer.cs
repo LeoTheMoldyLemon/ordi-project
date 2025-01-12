65efdf25bc15b36f1fb72032993b76efdec6a2be
leonardo simunovic <leonardo.simunovic@gmail.com>
2025-01-12 17:25:27 +0100

    lmao

--- Assets/Scripts/Systems/Audio/SFXPlayer.cs
+++ Assets/Scripts/Systems/Audio/SFXPlayer.cs
@@ -14,15 +14,3 @@ public class SFXPlayer : MonoBehaviour
     }
 
 }
-
-
-org.springframework.security.authorization.AuthorizationDeniedException: Access Denied
-\tat org.springframework.security.authorization.method.ThrowingMethodAuthorizationDeniedHandler.handleDeniedInvocation(ThrowingMethodAuthorizationDeniedHandler.java:38)
-\tat org.springframework.security.authorization.method.AuthorizationManagerBeforeMethodInterceptor.handle(AuthorizationManagerBeforeMethodInterceptor.java:290)
-\tat org.springframework.security.authorization.method.AuthorizationManagerBeforeMethodInter…at org.apache.tomcat.util.net.NioEndpoint$SocketProcessor.doRun(NioEndpoint.java:1741)
-\tat org.apache.tomcat.util.net.SocketProcessorBase.run(SocketProcessorBase.java:52)
-\tat org.apache.tomcat.util.threads.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1190)
-\tat org.apache.tomcat.util.threads.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:659)
-\tat org.apache.tomcat.util.threads.TaskThread$WrappingRunnable.run(TaskThread.java:63)
-\tat java.base / java.lang.Thread.run(Thread.java:1575)
-"
